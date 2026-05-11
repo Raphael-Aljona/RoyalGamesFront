76a258a3f5203574ecbc581703bc8053761a247a
Raphael-Aljona <raphael.aljona@gmail.com>
2026-05-11 11:02:02 -0300

    First commit: Projeto criardo

---
+++ components/btn/btn.tsx
@@ -0,0 +1,11 @@
+import styles from "./btn.module.css"
+
+const Btn = () => {
+    return (
+        <button className={styles.botao} type="submit">
+            Entrar
+        </button>
+    )
+}
+
+export default Btn
---
+++ pages/login/index.tsx
@@ -0,0 +1,30 @@
+import styles from "./login.module.css"
+import Btn from "../../components/btn/btn";
+
+const Login = () => {
+    return (
+        <>
+            <main className={styles.main}>
+                <img src="/imgs/mulher%20login.png" alt=""/>
+
+                <div className={styles.form_login}>
+                    <img className={styles.logo} src="/imgs/Logo%20Header.png" alt=""/>
+                    <form className={styles.campos_login} action="">
+                        <div className={styles.campo_texto}>
+                            <label htmlFor="">Email</label>
+                            <input type="text"/>
+                        </div>
+                        <div className={styles.campo_texto}>
+                            <label htmlFor="">Senha</label>
+                            <input type="password"/>
+                        </div>
+                        <Btn></Btn>
+                    </form>
+                </div>
+
+            </main>
+        </>
+    )
+}
+
+export default Login
