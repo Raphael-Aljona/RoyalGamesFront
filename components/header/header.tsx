ba285003eb51aeef555f5d4d4bd8c85cf2c745d2
Raphael-Aljona <raphael.aljona@gmail.com>
2026-05-11 16:34:12 -0300

    feat: Estrutura de pastas com todas as páginas criadas

---
+++ components/header/header.tsx
@@ -0,0 +1,13 @@
+import styles from "./header.module.css"
+
+const Header = () => {
+    return (
+        <>
+            <header>
+                <img src="/imgs/Logo%20Header.png" alt=""/>
+            </header>
+        </>
+    );
+}
+
+export default Header;
