3d721358ca659e558ae4bf840f199a4dee9a60d1
Raphael-Aljona <raphael.aljona@gmail.com>
2026-05-15 10:50:01 -0300

    fix: Imagem vindo corretamente da api nos gets

--- pages/api/jogoService.ts
+++ pages/api/jogoService.ts
@@ -13,12 +13,28 @@ type Jogo = {
     adminUsuario: boolean,
 }
 
+type ListarJogo = {
+    nome: string,
+    preco: number,
+    descricao: string,
+    imagemUrl: File | null,
+    usuarioIds: number[],
+    categoriaIds: number[],
+    plataformaID: number[],
+    classificacaoIndicativaIds: number[],
+    adminUsuario: boolean,
+}
+
 export async function getJogos() {
     try {
         const response = await api.get("/Jogo")
 
-        console.log(response.data);
-        return response.data;
+        const produtos = response.data.map((item:ListarJogo) => ({
+            ...item,
+            imagemUrl: `${api.defaults.baseURL}${item.imagemUrl}`,
+        }))
+
+        return produtos;
     } catch (e: any) {
         throw new Error(e.message);
     }
@@ -28,7 +44,6 @@ export async function getJogoById(id: number) {
     try {
         const response = await api.get(`/Jogo/${id}`);
 
-        console.log(response.data);
         return response.data;
     } catch (e: any) {
         throw new Error(e.message);
@@ -39,16 +54,6 @@ export async function cadastrarJogo(dados: Jogo) {
     const formData = new FormData();
 
     try {
-        console.log(`dados: ${dados.nome}`);
-        console.log(`dados: ${dados.adminUsuario}`);
-        console.log(`dados: ${dados.imagem}`);
-        console.log(`dados: ${dados.usuarioIds}`);
-        console.log(`dados: ${dados.preco}`);
-        console.log(`dados: ${dados.categoriaIds}`);
-        console.log(`dados: ${dados.classificacaoIndicativaIds}`);
-        console.log(`dados: ${dados.descricao}`);
-        console.log(`dados: ${dados.plataformaID}`);
-
         formData.append("Nome", dados.nome,);
         formData.append("Preco", dados.preco.toString(),);
         formData.append("Descricao", dados.descricao,);
